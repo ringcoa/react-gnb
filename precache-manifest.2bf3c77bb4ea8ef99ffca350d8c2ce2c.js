a54867968555c8e4d2968bbdbb7b0a3669fd9fbe
ringcoa <gkakdlv@gmail.com>
2020-08-27 11:41:16 +0900

    Updates

---
+++ precache-manifest.2bf3c77bb4ea8ef99ffca350d8c2ce2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69aa36e9fd0d8625d2a00beb664fce87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00cc5ae5339c2b159b90",
+    "url": "/static/css/main.7a14d149.chunk.css"
+  },
+  {
+    "revision": "a9393f1db42465c10105",
+    "url": "/static/js/2.08d19b96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.08d19b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00cc5ae5339c2b159b90",
+    "url": "/static/js/main.f0101a5a.chunk.js"
+  },
+  {
+    "revision": "480d6149425eada857bb",
+    "url": "/static/js/runtime-main.4e0abe5d.js"
+  }
+]);
